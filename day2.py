aeca3bdca808fe63b9db45e72f953da424ffa85d
thabeswar <thabeswar2003@gmail.com>
2024-02-18 20:39:12 +0530

    day2-of python

---
+++ day2.py
@@ -0,0 +1,12 @@
+v="happy coding:)"
+x=5
+y=9.0
+z=True
+print("declaration and initialization in python is done ")
+print(f"declared and initialized \nvalue of int x:{x} \nfloat y:{y} \nboolean z:{z}")
+print("user input type")
+print("enter your valuable data")
+a=str(input("enter your name:"))
+b=int(input("enter your age: "))
+print(f"hello {a} nice to meet you")
+
